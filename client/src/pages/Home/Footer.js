ec8873f2afc0f18d361e4f3e15cc1263fa340a63
shripal <shripalp@gmail.com>
2023-05-10 19:59:51 -0600

    footer/tab changed

--- client/src/pages/Home/Footer.js
+++ client/src/pages/Home/Footer.js
@@ -5,9 +5,9 @@ function Footer() {
     <div className="py-10">
       <div className="h-[1px] w-full bg-gray-700"></div>
       <div className="flex items-center justify-center flex-col mt-10 opacity-70">
-        <h1 className="text-white">Designed and developed by</h1>
+        <h1 className="text-white"></h1>
         <h1 className="text-white">
-          <span className="text-white">Shripal Parikh</span>
+          <span className="text-white">Jay Parikh</span>
         </h1>
       </div>
     </div>
